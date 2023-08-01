1d58ff0dba6dcef52fcc6a4f22929f29fbb60aeb
ericles-porty <ericlesdsantos@gmail.com>
2023-08-01 04:45:25 -0300

    hot: wip

--- api.py
+++ api.py
@@ -1,3 +1,5 @@
+import math
+import time
 from fastapi import FastAPI, Query
 from fastapi.responses import RedirectResponse
 from fastapi.openapi.utils import get_openapi
@@ -10,102 +12,150 @@ list_of_interests_available = [
 ]
 
 
-def get_path(start_id, goal_id, algorithm):
+def get_distance(x1, y1, x2, y2):
+    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+
+def get_path(start_id: int, goal_id: int, algorithm: str):
     path = []
-
-    path.append(start_id)
-    while start_id != goal_id:
-        directory = f"table_q/{algorithm}/table_q_{goal_id}.csv"
-        _file = open(directory, "r", encoding="utf-8")
-        for line in _file.readlines():
-            line_split = line.split(",")
-            if line_split[0] == str(start_id):
-                start_id = int(line_split[1])
-                path.append(start_id)
-                break
-
-        _file.close()
-
-    return path
-
-
-def get_path_interest(start_id: int, goal_id: int, categories: list,
-                      algorithm: str):
-    interest_vertexes = []
     total_distance = 0
-    # Get all vertexes with the same category
-    _file = open("vertices.csv", "r", encoding="utf-8-sig")
+
+    # get the vertices
+    directory = "vertices.csv"
+    _file = open(directory, "r", encoding="utf-8-sig")
+    vertices_dict = {}
     for line in _file.readlines():
         line_split = line.split(",")
-        print (line_split[2].lower(), categories)
-        if str(line_split[2]).lower() in categories:
-            interest_vertexes.append(int(line_split[0]))
+        vertices_dict[line_split[0]] = {
+            "pos_x": float(line_split[3]),
+            "pos_y": float(line_split[4])
+        }
 
-    list_of_tables = []
+    # create the goal table
+    directory = f"table_q/{algorithm}/table_q_{goal_id}.csv"
+    _file = open(directory, "r", encoding="utf-8-sig")
+    table = {}
+    for line in _file.readlines():
+        line_split = line.split(",")
+        table[line_split[0]] = {
+            "next_vertex": line_split[1],
+        }
 
-    # Get all tables of the interest vertexes
-    for vertex in interest_vertexes:
-        directory = f"table_q/{algorithm}/table_q_{vertex}.csv"
-        _file = open(directory, "r", encoding="utf-8-sig")
-        table = []
-        for line in _file:
-            line = line.split(",")
-            table.append(line)
-        list_of_tables.append(table)
+    # path from start to goal
+    current = start_id
+    while current != goal_id:
+        distance = get_distance(
+            vertices_dict[str(current)]["pos_x"],
+            vertices_dict[str(current)]["pos_y"],
+            vertices_dict[str(table[str(current)]["next_vertex"])]["pos_x"],
+            vertices_dict[str(table[str(current)]["next_vertex"])]["pos_y"])
+        current = int(table[str(current)]["next_vertex"])
+        path.append(current)
+        total_distance += distance
+
+    return path, total_distance
+
+
+def get_path_interest_new(start_id: int, goal_id: int, n_interests: int,
+                          interests: list, algorithm: str):
+    total_distance = 0
+
+    vertices_dict = {}
+    directory = "vertices.csv"
+    _file = open(directory, "r", encoding="utf-8-sig")
+    for line in _file.readlines():
+        line_split = line.split(",")
+        vertices_dict[line_split[0]] = {
+            "category": line_split[2],
+            "pos_x": float(line_split[3]),
+            "pos_y": float(line_split[4])
+        }
+
+    interest_vertexes_dict = {}
+
+    # Get all vertexes with the same category
+    for vertex_id in vertices_dict:
+        if vertex_id == start_id or vertex_id == goal_id:
+            continue
+
+        if vertices_dict[vertex_id]["category"].lower() in interests:
+            interest_vertexes_dict[vertex_id] = vertices_dict[vertex_id]
+
+    n_iterations = n_interests if n_interests < len(
+        interest_vertexes_dict) else len(interest_vertexes_dict)
 
     path = []
     path.append(start_id)
     current = start_id
-
+    print(interest_vertexes_dict)
+    print(current)
     # while list of interest vertexes is not empty
-    while len(interest_vertexes) > 0:
-        bigger_q = 0
-        interest_index = 0
-        for i in range(len(list_of_tables)):
-            if float(list_of_tables[i][current][2]) > bigger_q:
-                bigger_q = float(list_of_tables[i][current][2])
-                interest_index = i
+    for i in range(n_iterations):
+        smaller_distance = 9999
+        shortest_vertex = None
+
+        # find the next shortest interest vertex
+        for vertex_id in interest_vertexes_dict:
+            distance = get_distance(vertices_dict[str(current)]["pos_x"],
+                                    vertices_dict[str(current)]["pos_y"],
+                                    vertices_dict[vertex_id]["pos_x"],
+                                    vertices_dict[vertex_id]["pos_y"])
+            if distance < smaller_distance:
+                smaller_distance = distance
+                shortest_vertex = vertex_id
 
         # while current vertex is not the interest vertex
-        while current != interest_vertexes[interest_index]:
-            current = int(list_of_tables[interest_index][current][1])
-            path.append(current)
-            total_distance += float(
-                list_of_tables[interest_index][current][2])
+        while current != int(shortest_vertex):
+            directory = f"table_q/{algorithm}/table_q_{shortest_vertex}.csv"
+            _file = open(directory, "r", encoding="utf-8-sig")
+            for line in _file.readlines():
+                line_split = line.split(",")
+                if line_split[0] == str(current):
+                    distance = get_distance(
+                        vertices_dict[str(current)]["pos_x"],
+                        vertices_dict[str(current)]["pos_y"],
+                        vertices_dict[line_split[1]]["pos_x"],
+                        vertices_dict[line_split[1]]["pos_y"])
+                    current = int(line_split[1])
+                    total_distance += distance
+                    path.append(current)
+                    break
 
-        list_of_tables.pop(interest_index)
-        interest_vertexes.pop(interest_index)
+        interest_vertexes_dict.pop(shortest_vertex)
 
     # append the goal table
     directory = f"table_q/{algorithm}/table_q_{goal_id}.csv"
     _file = open(directory, "r", encoding="utf-8-sig")
-    table = []
-    for line in _file:
-        line = line.split(",")
-        table.append(line)
-    list_of_tables.append(table)
+    table = {}
+    for line in _file.readlines():
+        line_split = line.split(",")
+        table[line_split[0]] = {
+            "next_vertex": line_split[1],
+        }
 
     # at the end, append the goal path
     while current != goal_id:
-        current = int(list_of_tables[0][current][1])
+        distance = get_distance(
+            vertices_dict[str(current)]["pos_x"],
+            vertices_dict[str(current)]["pos_y"],
+            vertices_dict[str(table[str(current)]["next_vertex"])]["pos_x"],
+            vertices_dict[str(table[str(current)]["next_vertex"])]["pos_y"])
+        current = int(table[str(current)]["next_vertex"])
         path.append(current)
-        total_distance += float(list_of_tables[0][current][2])
+        total_distance += distance
 
-    print("Path: ", path)
-    print("Quantidade de passos: ", len(path))
-    print(f"Distância percorrida em metros: {total_distance:.2f}")
-    return path
-
+    return path, total_distance
 
 # full_run(algorithm=QLearningAgent)
 
 app = FastAPI(swagger_ui_parameters={"defaultModelsExpandDepth": -1})
 
 
-@app.get("/path/{id_origin}/{id_target}", tags=["Path"])
+@app.get("/path/{id_origin}/{id_target}/{n_interests}", tags=["Path"])
 async def get_path_request(
     id_origin: int,
     id_target: int,
+    n_interests: int,
     algorithm: Literal["QLearning", "Sarsa", "Astar", "BFS",
                        "DFS"] = "QLearning",
     interests: str | None = Query(
@@ -115,20 +165,42 @@ async def get_path_request(
         + ", ".join(list_of_interests_available))):
 
     path = []
-    if interests is None or algorithm.lower() in ["astar", "bfs", "dfs"]:
-        path = get_path(start_id=id_origin,
-                        goal_id=id_target,
-                        algorithm=algorithm.lower())
-    else:
+
+    if interests is None:
+        path, total_distance = get_path(start_id=id_origin,
+                                        goal_id=id_target,
+                                        algorithm=algorithm.lower())
+
+    if interests is not None:
         interests = interests.split(",")
-        interests = [interest.strip().lower() for interest in interests]
+        interests = [interest.strip().lower() for interest in interests
+                     ]  # remove spaces and convert to lowercase
 
-        path = get_path_interest(start_id=id_origin,
-                                 goal_id=id_target,
-                                 categories=interests,
-                                 algorithm=algorithm.lower())
+    path, total_distance = get_path_interest_new(start_id=id_origin,
+                                                 goal_id=id_target,
+                                                 n_interests=n_interests,
+                                                 interests=interests,
+                                                 algorithm=algorithm.lower())
 
-    return {"path": path}
+    # if algorithm.lower() in ["astar", "bfs", "dfs"]:
+    #     path = get_path(start_id=id_origin,
+    #                     goal_id=id_target,
+    #                     algorithm=algorithm.lower())
+    # else:
+
+    #     # path = get_path_interest(start_id=id_origin,
+    #     #                          goal_id=id_target,
+    #     #                          categories=interests,
+    #     #                          algorithm=algorithm.lower())
+    #     path, total_distance = get_path_interest_new(
+    #         start_id=id_origin,
+    #         goal_id=id_target,
+    #         n_interests=n_interests,
+    #         categories=interests,
+    #         algorithm=algorithm.lower())
+    #     print(f"Distância percorrida em metros: {total_distance:.2f}")
+
+    return {"path": path, "distance": total_distance, "steps": len(path)}
 
 
 @app.get("/", include_in_schema=False)
